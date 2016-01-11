4febd23507e2636d23e9c93c6c792da671724aca
Harry Gallagher <harrygallagher4@gmail.com>
2016-01-11 16:21:14 -0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
+(function ($) {
+    var s = skrollr.init({
+        forceHeight: false,
+        smoothScrolling: true,
+        render: function (data) {
+        }
+    });
+})(jQuery);
